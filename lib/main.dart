fe02c576ffa71bb8e3ecc36c7ba504028a0cd5fe
biryang <56025411+biryang@users.noreply.github.com>
2021-03-21 00:27:50 +0900

    route 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:weight_diary/components/custom_floating.dart';
+import 'package:weight_diary/models/diary.dart';
 import 'package:weight_diary/screens/add_screen.dart';
-import 'package:weight_diary/screens/chart_screen.dart';
+import 'package:weight_diary/screens/history_screen.dart';
 import 'package:weight_diary/screens/main_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
 
 import 'models/diary_data.dart';
 
 void main() {
+  Hive.registerAdapter(DiaryAdapter());
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: ChangeNotifierProvider(
-        create: (context) => DiaryData(),
-        child: SafeArea(
-          child: Material(
-            child: Scaffold(
-              body: PageView(
-                  onPageChanged: (value) {
-                    print(value);
-                  },
-                  children: [
-                    MainScreen(),
-                    AddScreen(),
-                    HistoryScreen(),
-                  ]),
-              floatingActionButton:true ? CustomFloating() : null,
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerFloat,
-            ),
-          ),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => DiaryData(),
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        routes: {
+          '/' : (context) => MainScreen(),
+          '/add':(context)=>AddScreen(),
+        },
+        initialRoute: '/',
       ),
     );
   }
