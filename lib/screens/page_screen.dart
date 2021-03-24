6f1d4d8f9de076cb7548a47c674098c31e872e8c
biryang <56025411+biryang@users.noreply.github.com>
2021-03-24 21:21:39 +0900

    페이지 스크린 추가

---
+++ lib/screens/page_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:weight_diary/screens/history_screen.dart';
+import 'package:weight_diary/screens/main_screen.dart';
+
+class PageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        MainScreen(),
+        HistoryScreen()
+      ],
+    );
+  }
+}
